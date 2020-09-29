88defeb0a74be5e0e213748ac87cc24458640d1e
Simeon <drsimeon_87@abv.bg>
2020-09-29 10:43:23 +0300

    delete to do manual fix for pr

--- Songs/src/main/java/Application.java
+++ Songs/src/main/java/Application.java
@@ -5,43 +5,5 @@ public class Application {
 
     public static void main(String[] args) {
 
-        /*
-         * Creating sample songs
-         */
-        Song pointsOfAuthority = new Song("Points of authority", "Rock", 267, "Lorem, ipsum ....");
-        Song summerChillOut = new Song("Summer chill out", "Pop", 267, "Random lyrics for the random song");
-        Song dubstep = new Song("Metamorphosis", "Dubstep", 267, "This song has no Lyrics");
-
-        /*
-         * Creating sample author
-         */
-        Author chesterBenington = new Author("Chester Benington", 33);
-
-        chesterBenington.addSong(pointsOfAuthority);
-        chesterBenington.addSong(summerChillOut);
-        chesterBenington.addSong(dubstep);
-
-        /*
-         * Manipulations with songs and their methods
-         */
-        System.out.println(pointsOfAuthority.toString());
-
-        System.out.println("Genre to compare - Rock\n" +
-                pointsOfAuthority.checkIfGenresMatch("Rock") + "\n");
-
-        System.out.println("Genre to compare - pop\n" +
-                pointsOfAuthority.checkIfGenresMatch("pop") + "\n");
-
-        System.out.println(pointsOfAuthority.printSongDuration() + "\n");
-
-        System.out.println("Lyrics:\n" +
-                pointsOfAuthority.printSongLyrics() + "\n");
-
-        /*
-         * Manipulation with authors and their methods
-         */
-        System.out.println(chesterBenington.toString() + "\n");
-
-        System.out.println(chesterBenington.listAuthorsSongs());
     }
 }
--- Songs/src/main/java/Author.java
+++ Songs/src/main/java/Author.java
@@ -7,43 +7,5 @@ import java.util.stream.Collectors;
  * Has methods to print author's songs, authors name and age, add song
  */
 public class Author {
-    private List<Song> songs = new ArrayList<>();
-    private String name;
-    private int age;
-
-    public Author(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    /**
-     * @return String - Author details : name and age
-     */
-    @Override
-    public String toString() {
-        return "Author:" +
-                "name: '" + name + '\'' +
-                ", age: " + age;
-    }
-
-    /**
-     * @return String - author's name and his songs listed below on new line
-     */
-    public String listAuthorsSongs() {
-        return "Author name: " +
-                this.name +
-                ",\nList of songs:" +
-                System.lineSeparator() +
-                songs.stream()
-                        .map(Song::getTitle)
-                        .collect(Collectors.joining("\n"));
-    }
-
-    /**
-     * @param song Adds a song to the author's collection of songs
-     */
-    public void addSong(Song song) {
-        songs.add(song);
-    }
 
 }
